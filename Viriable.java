2089742707486f625fa618f9a662774a27054d5c
Vitaliy <s-beat@yandex.ru>
2019-09-18 21:08:53 +0300

    Changes

--- Viriable.java
+++ Viriable.java
@@ -1,22 +1,20 @@
 public class Viriable {
 
 	public static void main(String[] args) {
-
 		short cpu = 2400;
 		byte core = 2;
-		int soc = 988;
-		long mem = 8000;
-		float freq = 99.76F;
-		double tactfreq = 2294.5;
+		int socet = 988;
+		long memory = 8000;
+		float frequnce = 99.76F;
+		double tactFrequnce = 2294.5;
 		char brand = 'i';
-
+		
 		System.out.println("Frequnce CPU: " + cpu);
 		System.out.println("Core: " + core);
-		System.out.println("Socet: " + soc);
-		System.out.println("Memory: " + mem);
-		System.out.println("Frequnce: " + freq);
+		System.out.println("Socet: " + socet);
+		System.out.println("Memory: " + memory);
+		System.out.println("Frequnce: " + frequnce);
 		System.out.println("Brand: " + brand);
-		System.out.println("Memory: " + mem);
-		
+		System.out.println("Current frequency: " + tactFrequnce);
 	}
 }
