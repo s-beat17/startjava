439f4a4ba0e4e338f88dee7f4ce5aded11e46f2a
Vitaliy <s-beat@yandex.ru>
2019-09-18 17:28:06 +0300

    Secondary commit

---
+++ Viriable.java
@@ -0,0 +1,22 @@
+public class Viriable {
+
+	public static void main(String[] args) {
+
+		short cpu = 2400;
+		byte core = 2;
+		int soc = 988;
+		long mem = 8000;
+		float freq = 99.76F;
+		double tactfreq = 2294.5;
+		char brand = 'i';
+
+		System.out.println("Frequnce CPU: " + cpu);
+		System.out.println("Core: " + core);
+		System.out.println("Socet: " + soc);
+		System.out.println("Memory: " + mem);
+		System.out.println("Frequnce: " + freq);
+		System.out.println("Brand: " + brand);
+		System.out.println("Memory: " + mem);
+		
+	}
+}
