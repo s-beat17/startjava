008b789df306052bdfa7ed25823338b4c7252ec3
s-beat17 <s-beat@yandex.ru>
2019-09-25 21:22:07 +0300

    Changes3

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,10 @@
+public class Unicode {
+
+	public static void main(String[] args) {
+		char simbol = 33;
+		for(int i=33; i<126; i++) {
+		System.out.println("Unicode: " + simbol);
+		simbol++;
+		}
+	}
+}
